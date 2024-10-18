66711e576e2143bb03cebfe71c19fabc9e763a9e
Marko Markovic <markomarkoviccb@gmail.com>
2024-10-18 11:50:20 +0200

    first commit

---
+++ src/main/java/com/user_admin/app/UserAdministrationApplicationExampleApplication.java
@@ -0,0 +1,13 @@
+package com.user_admin.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAdministrationApplicationExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAdministrationApplicationExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/user_admin/app/UserAdministrationApplicationExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user_admin.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAdministrationApplicationExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
